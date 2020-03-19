a80c2b346408f12b87b6189a294cdaba0ecbc657
Bui Duc Tuan <antivenomff@gmail.com>
2020-03-19 13:11:15 +0400

    first commit

---
+++ matran2.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
+#include <iomanip>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void Nhap(int a[][100],int m,int n);
+void Xuat(int a[][100],int m,int n);
+int tinhtong(int a[][100],int m,int n);
+void doicho(int a[][100],int m,int n);
+int main(int argc, char** argv) {
+	int a[100][100];
+	int m,n;
+	cout<<"Nhap so hang :";
+	cin>>m;
+	cout<<"Nhap so cot :";
+	cin>>n;
+	cout<<"Ma tran la : "<<endl;
+	Nhap(a,m,n);
+	Xuat(a,m,n);
+	tinhtong(a,m,n);
+	return 0;
+}
+void Nhap(int a[][100], int m, int n)
+{
+	int i, j;
+    srand(time(NULL));
+    for (i = 0; i < m; i++)
+        for (j = 0; j < n; j++)
+        {
+        	int dau = rand()&1 ?  -1 : 1;
+            a[i][j]= dau * rand()%19 +1;
+            
+        }
+}
+void Xuat(int a[][100], int m, int n)
+{
+	int i, j;
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        cout<<a[i][j]<<setw(9);
+        cout<<"\n";
+    }
+}
+int tinhtong(int a[][100],int m,int n)
+{
+	int i,j,indexMinElem = 0, minElem = 0;;
+    for (i = 0; i < m; i++)
+    {
+    	int sum = 0;
+    	indexMinElem = 0;  
+        minElem = a[i][indexMinElem];
+        for (j = 0; j < n; j++)
+        {
+        	if(a[i][j] > 0)
+            {
+         	  sum += a[i][j];
+		    }
+        	if (a[i][j] <= minElem)   
+            {
+                indexMinElem = j;   
+                minElem = a[i][indexMinElem]; 
+            }
+		}
+		cout<<"\nTong cac phan tu duong hang thu "<<i<<" : "<<sum;	   
+		a[i][n-1] = sum; 
+		a[i][indexMinElem] = sum; 
+    }
+    cout<<"\nMa tran sau la :\n";
+        for (i = 0; i < m; i++)
+         {
+            for (j = 0; j < n; j++)
+            cout<<a[i][j]<<setw(9);
+            cout<<"\n";
+         }
+}
+
